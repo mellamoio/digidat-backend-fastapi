6d428dbfff716bcdacf2f89c9dbea432517bfa37
Iosef Ayala <iosef.ayalahuaringa@gmail.com>
2025-12-22 23:41:41 -0500

    CRUD Contratista y Financista

--- backend/app/model/informacion_financista.py
+++ backend/app/model/informacion_financista.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.config.db import Base
@@ -10,6 +10,7 @@ class InformacionFinancista(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     id_responsable = Column(Integer, ForeignKey("usuarios.id_responsable"), nullable=False)
     id_obra = Column(Integer, ForeignKey("obras.id_obra"), nullable=False)
+    detalle = Column(Text, nullable=True)
     
     responsable = relationship("User", back_populates="informaciones_financistas")
     obra = relationship("Obra", back_populates="info_financista")
